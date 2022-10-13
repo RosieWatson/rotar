84d8c17f05533a3225e93fc6cb72e3a6230fa67b
Chee <chee@zeroheight.com>
2022-10-13 09:24:28 +0100

    Add form

--- src/containers/addPeopleContainer.jsx
+++ src/containers/addPeopleContainer.jsx
@@ -1,30 +1,63 @@
 import * as React from "react";
 import styled from "styled-components"
 import { useForm } from "react-hook-form";
-import { Checkbox, Button, TextField } from "react95";
+import { Checkbox, Button, TextField, Select } from "react95";
+
+const timezones = [
+  { value: "us", label: "US"},
+  { value: "uk", label: "UK"}
+]
 
 export const AddPeopleContainer = () => {
   const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const [addSuccess, setAddSuccess] = React.useState(false)
+
+  const handleAddAnother = () => {
+    setAddSuccess(false);
+  }
+
+  const onSubmit = (data) => {
+    console.log(data)
+    setAddSuccess(true);
+  };
 
   return (
     <main>
-      <h1>Add People</h1>
-      <FormContainer onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="name">Name</label>
-        <TextField id="name" {...register("name")} />
+      { !addSuccess ?
+        (
+          <>
+            <h1>Add a person</h1>
+            <FormContainer onSubmit={handleSubmit(onSubmit)}>
+              <label htmlFor="name">Name</label>
+              <TextField id="name" {...register("name")} />
 
-        <label htmlFor="email">Email</label>
-        <TextField id="email" {...register("email")}/>
+              <label htmlFor="email">Email</label>
+              <TextField id="email" {...register("email")}/>
 
-        <label htmlFor="timezone">Timezone</label>
-        <TextField id="timezone" {...register("timezone")}/>
+              <label htmlFor="timezone">Timezone</label>
+              <Select
+                id="timezone"
+                options={timezones}
+                defaultValue="uk"
+                {...register("timezone")}/>
 
+              <Checkbox
+                id="office"
+                label="Will they be in the office?"
+                {...register("office")}
+              />
 
-        <Checkbox id="office" label="Will they be in the office?" {...register("office")}/>
+              <Button type="submit">Add to Rota</Button>
+            </FormContainer>
+          </>
+        ) : (
+          <>
+            <h1>Success!</h1>
+            <Button type="button" onClick={handleAddAnother}>Add another person</Button>
+          </>
+        )
+      }
 
-        <Button type="submit">Add to Rota</Button>
-      </FormContainer>
     </main>);
 }
 
