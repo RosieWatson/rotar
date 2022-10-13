67cfa0fe2d9481743ba7aa2531096e02ace174a5
Rosie Watson <rosiewatson@icloud.com>
2022-10-13 12:21:10 +0300

    add more data

--- src/data.js
+++ src/data.js
@@ -55,13 +55,68 @@ export const data = {
       email: 'adam@zeroheight.com',
       timezone: 'gmt',
       office: true
-    }
-    ,
+    },
     {
       id: 9,
       name: 'Olivia Diallo',
       email: 'olivia@zeroheight.com',
       timezone: 'pdt',
+      office: false
+    },
+    {
+      id: 10,
+      name: 'Seth Corker',
+      email: 'seth@zeroheight.com',
+      timezone: 'gmt',
+      office: true
+    },
+    {
+      id: 11,
+      name: 'Paula Blackburn',
+      email: 'paula@zeroheight.com',
+      timezone: 'gmt',
+      office: false
+    },
+    {
+      id: 12,
+      name: 'Sufi Gaffar',
+      email: 'sufi@zeroheight.com',
+      timezone: 'gmt',
+      office: true
+    },
+    {
+      id: 13,
+      name: 'Paul McKenna',
+      email: 'paul@zeroheight.com',
+      timezone: 'gmt',
+      office: true
+    },
+    {
+      id: 14,
+      name: 'Olga Grabek',
+      email: 'olga@zeroheight.com',
+      timezone: 'eet',
+      office: false
+    },
+    {
+      id: 15,
+      name: 'Theo Evelyn-Rahr',
+      email: 'theo@zeroheight.com',
+      timezone: 'gmt',
+      office: false
+    },
+    {
+      id: 16,
+      name: 'Brienne Wong',
+      email: 'brienne@zeroheight.com',
+      timezone: 'pdt',
+      office: false
+    },
+    {
+      id: 17,
+      name: 'Dan Donald',
+      email: 'dan@zeroheight.com',
+      timezone: 'gmt',
       office: false
     }
   ]
