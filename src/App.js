f3e91d7d8aa003505c793dd46f762be837ee5a52
Rosie Watson <rosie.w@zeroheight.com>
2022-10-13 12:31:35 +0300

    Pascal CASE

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ import original from 'react95/dist/themes/original';
 // original Windows95 font (optionally)
 import ms_sans_serif from 'react95/dist/fonts/ms_sans_serif.woff2';
 import ms_sans_serif_bold from 'react95/dist/fonts/ms_sans_serif_bold.woff2';
-import { GenerateContainer } from './containers/generateContainer';
-import { AddPeopleContainer } from "./containers/addPeopleContainer";
+import { GenerateContainer } from './containers/GenerateContainer';
+import { AddPeopleContainer } from "./containers/AddPeopleContainer";
 import { HomeContainer } from './containers/HomeContainer';
 import { importDataIntoLocalStorage } from "./dataUtils";
 import { TaskBar, List, Tree } from "@react95/core";
